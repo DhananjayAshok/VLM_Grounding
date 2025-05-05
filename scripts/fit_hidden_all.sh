9d36c69a70a339a2a10d39d11ec60fcb24a018b2
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-05-05 14:42:54 -0700

    echo

--- scripts/fit_hidden_all.sh
+++ scripts/fit_hidden_all.sh
@@ -1,7 +1,9 @@
 datasets=("mnist" "cifar100" "food101" "landmarks ""okvqa")
 
 for dataset in "${datasets[@]}"; do
+    echo "XXX Fitting on $dataset XXX"
     python main.py fit_hidden_states --datasets "$dataset"
 done
 
+echo "Fitting OOD"
 python main.py fit_hidden_states --datasets cifar100 --datasets landmarks --datasets food101 --datasets okvqa
